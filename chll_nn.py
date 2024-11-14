bfd875855d5e8d7c5f7ee392e781b05e9857b38a
JhonJHerrera <142182729+JhonJHerrera@users.noreply.github.com>
2024-11-14 10:33:42 -0400

    cleaning_code

--- chll_nn.py
+++ chll_nn.py
@@ -1,88 +1,69 @@
-import datetime   
-from datetime import datetime             
-import os                                 
-from pathlib import Path                  
-import shutil                             
-import eumdac                             
-import xarray as xr                       
-import matplotlib.pyplot as plt           
-import numpy as np                        
-import eumartools                         
-from shapely import geometry, vectorized  
+import datetime
+from datetime import datetime
+import os
+from pathlib import Path
+import shutil
+import eumdac
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+import eumartools
+from shapely import geometry, vectorized
 import csv
 import time
 import pandas as pd
 
-
-def download(directories, start_date, end_date, datastore, output_dir, roi, collection_ids = ["EO:EUM:DAT:0407", "EO:EUM:DAT:0556"]):
-    list_prod =[]
+def download(directories, start_date, end_date, datastore, output_dir, roi, collection_ids=["EO:EUM:DAT:0407", "EO:EUM:DAT:0556"]):
+    list_prod = []
     lines = []
     total_time = 0
 
-    
-    
-    # Directory to save the products (same directory for CSV)
-    if output_dir is None:
-        output_dir = os.path.join(os.getcwd(), 'products')
-
+    # Set output directory for products and CSV if not provided
+    output_dir = output_dir or os.path.join(os.getcwd(), 'products')
     os.makedirs(output_dir, exist_ok=True)
-    
-    # Get the data collection
-    for collectionID in collection_ids:
-        selected_collection = datastore.get_collection(collectionID)
-        # Search for products in the ROI and within the date range
+
+    for collection_id in collection_ids:
+        selected_collection = datastore.get_collection(collection_id)
+        
+        # Search for products within date range and ROI
         products = selected_collection.search(
             geo='POLYGON(({}))'.format(','.join(["{} {}".format(*coord) for coord in roi])),
             dtstart=start_date,
             dtend=end_date
         )
+
         if not products:
             print("No products found for the specified date and region.")
-        else:
-            # Loop through the found products
-            for product in products:
-                list_prod.append(product)
-        for product in list_prod:
-            start_time = time.time()
-            
-            # Look for the 'chl_nn.nc' and 'chl_oc4me.nc' files within the product
-            for entry in product.entries:
-                entry_name = 'entry'
-                entry_parts = entry.split('_')
-                if len(entry_parts) > 7:
-                    entry_name = entry_parts[7]
-                    if entry_name not in directories:
-                        directories.append(entry_name)
-                        print(entry_name)
+            continue
 
-                        # Append each new directory to 'directories_list.txt' without overwriting
-                        with open(os.path.join(output_dir, 'directories_list.txt'), 'a') as file:
-                            file.write(f"{entry_name}\n")
+        # Process each found product
+        for product in products:
+            list_prod.append(product)
+            
+            for entry in product.entries:
+                entry_name = entry.split('_')[7] if len(entry.split('_')) > 7 else 'entry'
                 
-                # Ensure the directory for the product entry exists
+                if entry_name not in directories:
+                    directories.append(entry_name)
+                    with open(os.path.join(output_dir, 'directories_list.txt'), 'a') as file:
+                        file.write(f"{entry_name}\n")
+
                 entry_dir = os.path.join(output_dir, entry_name)
                 os.makedirs(entry_dir, exist_ok=True)
 
-                if "Oa01_reflectance" in entry:
-                    required_entry = entry
-                if 'tie_geo_coordinates.nc' in entry:
-                    continue
-                if 'geo_coordinates.nc' in entry or 'chl' in entry:
-                    with product.open(entry=entry) as fsrc, open(os.path.join(output_dir, entry_name, fsrc.name),
-                                                                mode='wb') as fdst:
-                        print(f'Downloading {fsrc.name} in {os.path.join(output_dir, entry_name)}')
+                # Check and download specific files
+                if "Oa01_reflectance" in entry or 'geo_coordinates.nc' in entry or 'chl' in entry:
+                    with product.open(entry=entry) as fsrc, open(os.path.join(entry_dir, fsrc.name), 'wb') as fdst:
+                        print(f'Downloading {fsrc.name} to {entry_dir}')
                         shutil.copyfileobj(fsrc, fdst)
-                        print(f'Download of file {fsrc.name} finished.')
-                
+                        print(f'Download of file {fsrc.name} completed.')
+
                 # Break if necessary files are downloaded
-                if os.path.exists(os.path.join(output_dir, entry_name, 'geo_coordinates.nc')) and \
-                   os.path.exists(os.path.join(output_dir, entry_name, 'chl_nn.nc')) and \
-                   os.path.exists(os.path.join(output_dir, entry_name, 'chl_oc4me.nc')):
+                if all(os.path.exists(os.path.join(entry_dir, fname)) for fname in ['geo_coordinates.nc', 'chl_nn.nc', 'chl_oc4me.nc']):
                     break
 
-            # Record iteration time
-            end_time = time.time()
-            elapsed_time = end_time - start_time
+            # Track time per product download
+            elapsed_time = time.time() - start_time
             total_time += elapsed_time
             lines.append(f"Iteration {product}: {elapsed_time:.2f} seconds\n")
 
@@ -90,31 +71,24 @@ def download(directories, start_date, end_date, datastore, output_dir, roi, coll
     with open(os.path.join(output_dir, 'time_download'), 'w') as file:
         file.writelines(lines)
 
-    print(f"Tiempos de cada iteración guardados")
+    print("Download times saved")
     return directories
 
 
 def read_directories_list(output_dir):
-    """
-    Lee el archivo directories_list.txt y devuelve una lista de directorios.
-
-    Parameters:
-    - output_dir: Ruta del directorio donde se encuentra el archivo directories_list.txt.
-
-    Returns:
-    - Una lista de nombres de directorios leídos desde el archivo.
-    """
+    """Reads 'directories_list.txt' to retrieve list of directories."""
     directories = []
     file_path = os.path.join(output_dir, 'directories_list.txt')
-    
+
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             directories = [line.strip() for line in file.readlines()]
-        print(f"Directorio cargado desde {file_path}")
+        print(f"Directories loaded from {file_path}")
     else:
-        print(f"El archivo {file_path} no existe.")
+        print(f"{file_path} does not exist.")
     
     return directories
+
 
 def process_chlorophyll_data(vec, roi, directory=None):
     total_time = 0
@@ -123,34 +97,33 @@ def process_chlorophyll_data(vec, roi, directory=None):
 
     for paths in vec:
         start_time = time.time()
-        if directory is None:
-            directory = os.path.join(os.getcwd(), 'products', paths)
+        directory = directory or os.path.join(os.getcwd(), 'products', paths)
         os.makedirs(directory, exist_ok=True)
 
-        chl_path = os.path.join(directory, paths, 'chl_nn.nc')
-        geo_path = os.path.join(directory, paths, 'geo_coordinates.nc')
-        chl_oc4me_path = os.path.join(directory, paths, 'chl_oc4me.nc')
+        chl_path, geo_path, chl_oc4me_path = (
+            os.path.join(directory, paths, 'chl_nn.nc'),
+            os.path.join(directory, paths, 'geo_coordinates.nc'),
+            os.path.join(directory, paths, 'chl_oc4me.nc')
+        )
 
-        if not os.path.exists(chl_path) or not os.path.exists(geo_path):
-            print(f"Required files not found in {directory}. Skipping this directory.")
+        if not all(os.path.exists(path) for path in [chl_path, geo_path]):
+            print(f"Required files missing in {directory}. Skipping this directory.")
             continue
 
         try:
-            # Intentar abrir geo_coordinates.nc para confirmar que no esté corrupto
+            # Load and filter geo-coordinates
             geo_data = xr.open_dataset(geo_path)
-            lat = geo_data['latitude'].data
-            lon = geo_data['longitude'].data
-            geo_data.close()
-
+            lat, lon = geo_data['latitude'].data, geo_data['longitude'].data
             polygon = geometry.Polygon(roi)
             point_mask = vectorized.contains(polygon, lon, lat)
+            geo_data.close()
 
+            # Process chlorophyll data
             chl_data = xr.open_dataset(chl_path)
             chl_values = chl_data['CHL_NN'].data
             chl_data.close()
             chl_roi = chl_values[point_mask]
-            lat_roi = lat[point_mask]
-            lon_roi = lon[point_mask]
+            lat_roi, lon_roi = lat[point_mask], lon[point_mask]
 
             df = pd.DataFrame({
                 'latitude': lat_roi,
@@ -160,28 +133,24 @@ def process_chlorophyll_data(vec, roi, directory=None):
 
             if os.path.exists(chl_oc4me_path):
                 chl_oc4me_data = xr.open_dataset(chl_oc4me_path)
-                chl_oc4me_values = chl_oc4me_data['CHL_OC4ME'].data
+                df['chlorophyll_oc4me'] = chl_oc4me_data['CHL_OC4ME'].data[point_mask]
                 chl_oc4me_data.close()
-                chl_oc4me_roi = chl_oc4me_values[point_mask]
-                df['chlorophyll_oc4me'] = chl_oc4me_roi
 
+            # Save data to CSV and clean up
             output_path = os.path.join(directory, paths, 'chlorophyll_roi.csv')
             df.to_csv(output_path, index=False)
             print(f"DataFrame saved to: {output_path}")
 
-            if os.path.exists(chl_path):
-                os.remove(chl_path)
-            if os.path.exists(chl_oc4me_path):
-                os.remove(chl_oc4me_path)
-            if os.path.exists(geo_path):
-                os.remove(geo_path)
+            for file in [chl_path, chl_oc4me_path, geo_path]:
+                if os.path.exists(file):
+                    os.remove(file)
 
         except Exception as e:
             print(f"Error processing files in {directory}: {e}")
             continue
 
-        end_time = time.time()
-        elapsed_time = end_time - start_time
+        # Track time per directory processing
+        elapsed_time = time.time() - start_time
         total_time += elapsed_time
         lines.append(f"Iteration {paths}: {elapsed_time:.2f} seconds\n")
 
@@ -191,66 +160,37 @@ def process_chlorophyll_data(vec, roi, directory=None):
 
     return df
 
-def main(longps = -117.31646, latgps = 32.92993, factor = 0.01802):
+
+def main(longps=-117.31646, latgps=32.92993, factor=0.01802, sd = "2019-06-01", ed = "2019-06-03"):
     download_dir = os.path.join(Path.home(), "products")
     os.makedirs(download_dir, exist_ok=True)
 
-    # load credentials
     eumdac_credentials_file = Path(Path.home() / '.eumdac' / 'credentials')
 
+    # Load or create credentials
     if os.path.exists(eumdac_credentials_file):
         consumer_key, consumer_secret = Path(eumdac_credentials_file).read_text().split(',')
     else:
-        # creating authentication file
-        consumer_key = 'fNdGKRIUrnD_gUGgOofwyuXzFZAa'
-        # consumer_key = input('Enter your consumer key: ')
-        consumer_secret = 'ajdq2MjBDqGB5R_u51w1FSXLfKoa'
-        # consumer_secret = getpass.getpass('Enter your consumer secret: ')
-        try:
-            os.makedirs(os.path.dirname(eumdac_credentials_file), exist_ok=True)
-            with open(eumdac_credentials_file, "w") as f:
-                f.write(f'{consumer_key},{consumer_secret}')
-        except:
-            pass
-            
+        consumer_key, consumer_secret = 'fNdGKRIUrnD_gUGgOofwyuXzFZAa', 'ajdq2MjBDqGB5R_u51w1FSXLfKoa'
+        os.makedirs(os.path.dirname(eumdac_credentials_file), exist_ok=True)
+        with open(eumdac_credentials_file, "w") as f:
+            f.write(f'{consumer_key},{consumer_secret}')
+
     token = eumdac.AccessToken((consumer_key, consumer_secret))
-    print(f"This token '{token}' expires {token.expiration}")
+    print(f"Token '{token}' expires on {token.expiration}")
 
-    # create data store object
-    datastore = eumdac.DataStore(token)
-    #coordinates
-    longspf = longps + factor
-    longmpf = longps - factor
-    latpf = latgps + factor
-    latmf = latgps - factor
-    roi = [[longspf,latpf],[longmpf,latpf],[longmpf,latmf],[longspf,latmf],[longspf,latpf]]
-    # Pedir al usuario las fechas de inicio y fin para la descarga de productos
-    # start_date = input("Ingrese la fecha de inicio (YYYY-MM-DD): ")
-    # end_date = input("Ingrese la fecha de fin (YYYY-MM-DD): ")
-
-    # # Validar formato de fechas
-    # try:
-    #     datetime.strptime(start_date, '%Y-%m-%d')
-    #     datetime.strptime(end_date, '%Y-%m-%d')
-    # except ValueError:
-    #     print("Formato de fecha incorrecto. Por favor, use el formato YYYY-MM-DD.")
-    #     return
-    start_date = "2019-06-01"
-    end_date = "2019-06-03"
-    # Definir el ID de la colección
-    collectionID = 'EO:EUM:DAT:0407'
+    # Define ROI and collection dates
+    roi = [[longps + factor, latgps + factor], [longps - factor, latgps + factor],
+           [longps - factor, latgps - factor], [longps + factor, latgps - factor],
+           [longps + factor, latgps + factor]]
     
-    # Descargar productos y obtener los directorios de productos y ROI
     directories = read_directories_list(download_dir)
-    download(directories, start_date, end_date, datastore, download_dir, roi)
-    print(directories)
+    download(directories, sd, ed, eumdac.DataStore(token), download_dir, roi)
 
-    # Procesar datos de clorofila
     df = process_chlorophyll_data(directories, roi, download_dir)
-
-    # Mostrar el DataFrame final de clorofila
-    print("Datos de clorofila procesados:")
+    print("Processed chlorophyll data:")
     print(df)
+
 
 if __name__ == "__main__":
     main()
